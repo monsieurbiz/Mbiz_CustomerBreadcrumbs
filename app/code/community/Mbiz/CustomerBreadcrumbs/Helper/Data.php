d39c5011cfe1b5fffe7dda83d00719c56476e618
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2013-08-01 18:26:58 +0200

    Module

---
+++ app/code/community/Mbiz/CustomerBreadcrumbs/Helper/Data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Mbiz_CustomerBreadcrumbs for Magento.
+ *
+ * @license All rights reserved
+ * @author Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
+ * @category Mbiz
+ * @package Mbiz_CustomerBreadcrumbs
+ * @copyright Copyright (c) 2013 Monsieur Biz (http://monsieurbiz.com)
+ */
+
+/**
+ * Data Helper
+ * @package Mbiz_CustomerBreadcrumbs
+ */
+class Mbiz_CustomerBreadcrumbs_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+// Monsieur Biz Tag NEW_CONST
+
+// Monsieur Biz Tag NEW_VAR
+
+// Monsieur Biz Tag NEW_METHOD
+
+}
